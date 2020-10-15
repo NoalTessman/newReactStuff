a75488e332a65224e5fc6a445e64c809010e87e6
Noal Tessman <noaltessman@gmail.com>
2020-10-15 06:19:48 -0600

    Messed around with express, added better css to 90s portfolio

--- Projects/newprofile/src/App.js
+++ Projects/newprofile/src/App.js
@@ -5,12 +5,12 @@ function App() {
     <div className="App">
       <div className="section1"><p id="firstText">- Web Developer -</p></div>
       <div className="section2">
+        <div className="section2box">
         <p id="name">Noal Tessman</p>
         <p id="item">Projects</p>
         <p id="item2">Resume</p>
-        <p id="item">About</p>
-
-
+        <p id="item3">About</p>
+        </div>
       </div>
       <div className="section3"><p id="thirdText">- Software Engineer -</p></div>
     </div>
--- practice/expresstesting/index.js
+++ practice/expresstesting/index.js
@@ -1,11 +1,23 @@
+const members = require('./members');
 const express = require("express");
-
+const logger = require('./middleware/logger');
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hello World!!")
-});
+
+//Middleware
+app.use(logger);
+
+//Get Single Member
+app.get('/api/members/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id) )
+    
+    found ? res.json(members.filter(member => member.id === parseInt(req.params.id))):res.status(400).json({msg: `No Member of ID ${req.id} `});
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+})
+ //Gets all members
+ app.get('/api/members', (req,res) =>{res.json(members);} )
 
 const PORT = process.env.PORT | 5000;
 
-app.listen(PORT), () => console.log(`Server started on port ${PORT}`)
+app.listen(PORT), () => console.log(`Server started on port ${PORT}:`)
